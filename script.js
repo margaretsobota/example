60af29c99d1ab5e46a0886b9365460af365ae82e
Margot <margaretsobota@gmail.com>
2019-11-18 19:51:01 -0600

    saved changes

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function Add(){
+	var x = document.getElementById("num1").valueAsNumber;
+	var y = document.getElementById("num2").valueAsNumber;
+
+	document.getElementById("demo").innerHTML = x + y;
+}
+
+function Subtract(){
+	var x = document.getElementById("num1").valueAsNumber;
+	var y = document.getElementById("num2").valueAsNumber;
+
+	document.getElementById("demo").innerHTML = x - y;
+}
+
+function Divide(){
+	var x = document.getElementById("num1").valueAsNumber;
+	var y = document.getElementById("num2").valueAsNumber;
+
+	document.getElementById("demo").innerHTML = x / y;
+}
+
+function Multiply(){
+	var x = document.getElementById("num1").valueAsNumber;
+	var y = document.getElementById("num2").valueAsNumber;
+
+	document.getElementById("demo").innerHTML = x * y;
+}
+
+
+
+
+
